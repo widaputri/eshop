3cb1d90bee9efe63d2ddd8dae1368a0791600c8b
widaputri <widapk2709@gmail.com>
2025-03-14 14:27:34 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -2,11 +2,13 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+@Repository
 public class OrderRepository{
     private List<Order> orderData = new ArrayList<>();
 
