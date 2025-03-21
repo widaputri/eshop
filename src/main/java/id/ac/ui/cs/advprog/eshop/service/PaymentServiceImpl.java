96583d134fdf45a2528e71e0450b85f1e58a9e3f
widaputri <widapk2709@gmail.com>
2025-03-21 15:41:27 +0700

    [RED] Add PaymentService skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class PaymentServiceImpl implements PaymentService {
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData){
+        return null;
+    }
+
+    public Payment setStatus(Payment payment, String status){
+        return null;
+    }
+
+    public Payment getPayment(String paymentId) {
+        return null;
+    }
+
+    public List<Payment> getAllPayments(){
+        return null;
+    }
+}
